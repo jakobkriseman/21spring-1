33b5419bc897e5985a4f8a5cd2b55283c0b141f3
saas student <saasstudent@seattleacademy.software>
2017-05-03 18:52:15 +0000

    Add sensors to html

--- index.js
+++ index.js
@@ -5,36 +5,30 @@ var port = process.env.PORT || 3000;
 
 var nodeimu = require('nodeimu');
 var IMU = new nodeimu.IMU();
+
 function getData() {
     IMU.getValue(function(err, data) {
         if (err) throw err;
         sensors = data;
-	console.log(sensors);
-    });
-}
-getData();
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-function getData() {
-    IMU.getValue(function(err, data) {
-        if (err) throw err;
-        sensors = data;
-	console.log(sensors);
-	io.emit("sensors",sensors)
+        console.log(sensors);
+        io.emit("sensors", sensors)
     });
 }
 
-setInterval(getData,1000);
+setInterval(getData, 1000);
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+io.on('connection', function(socket) {
+
+
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
